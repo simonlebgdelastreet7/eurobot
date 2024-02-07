3df5b4a68a31b5275c6640767bb955bf2aad0e4d
simonlebgdelastreet7 <159255699+simonlebgdelastreet7@users.noreply.github.com>
2024-02-07 12:40:40 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,6 @@
-
+if (iscmons.button_is_pressed(pins.LED)) {
+    iscmons.servo_turn(pins.LED, 0, 2)
+    for (let index = 0; index < 1; index++) {
+        iscmons.servo_turn(pins.LED, 100, 15)
+    }
+}
